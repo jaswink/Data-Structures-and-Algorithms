682c01a791301be85b2007975bc4b9a36ab70af6
Jaswin Kaur <30419091+jaswink@users.noreply.github.com>
2020-05-19 06:10:02 -0400

    Delete quick_sort.h

--- quick_sort.h
+++
@@ -1,7 +0,0 @@
-#ifndef QUICK_SORT_H_INCLUDED
-#define QUICK_SORT_H_INCLUDED
-void swap(int *x,int *y);
-int partition (int a[], int low, int high);
-void quickSort(int a[], int low, int high);
-
-#endif // QUICK_SORT_H_INCLUDED
