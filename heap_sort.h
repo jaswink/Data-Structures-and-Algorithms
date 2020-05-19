36d7d9bb138c4618b935292ebe7c8266e8371918
Jaswin Kaur <30419091+jaswink@users.noreply.github.com>
2020-05-19 06:10:12 -0400

    Delete heap_sort.h

--- heap_sort.h
+++
@@ -1,7 +0,0 @@
-#ifndef HEAP_SORT_H_INCLUDED
-#define HEAP_SORT_H_INCLUDED
-
-void insert (int a[],int n);
-void deleteE (int a[],int n);
-void heapSort (int a[], int n);
-#endif // HEAP_SORT_H_INCLUDED
